74d29a45773dd5e31e68803bbdc3ddb7366a23a9
sujeong1234 <96823001+sujeong1234@users.noreply.github.com>
2022-08-11 15:32:24 +0900

    loginjs

--- join/join_action.php
+++ join/join_action.php
@@ -1,24 +1,28 @@
-<?php # form 데이터 읽어오기
-$id = $_POST["id"];
-$pw = $_POST["pw"];
-$l_name = $_POST["l_name"];
-$f_name = $_POST["f_name"];
-
-
+<?php
+# db 접속
+// connect.php 파일의 내용을 그대로 가져옴.
 $db_host = "localhost"; 
 $db_user = "bigeducation"; 
 $db_passwd = "dusrntlf0!";
 $db_name = "bigeducation"; 
-$conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
+
+$conn = new mysqli($db_host, $db_user, $db_passwd, $db_name);
+
+if($conn->connect_error) echo "<h2>접속에 실패하였습니다.</h2>";
+else echo "<h2>접속에 성공하였습니다.</h2>";
+
+# form 데이터 읽어오기
+$id = $_POST["id"];
+$pw = $_POST["pw"];
+$l_name= $_POST["l_name"];
+$f_name = $_POST["f_name"];
+
+echo "추가할 아이디 {$id}";
+
 # insert sql 작성
-$query = "INSERT INTO user (id, pw, l_name, f_name) VALUES('$id', '$pw', '$l_name', '$f_name')";
-$resut=mysqli_query($con,$query);
-if(!$result) 
-{?>
-    <script> alert('회원가입이 완료되었습니다.'); location.href=".."; </script> 
-<?php
-} else {?>
-    <script> alert('회원가입에 실패했습니다.\n다시 시도해 주세요.'); location.href=".."; </script>
-<?php } ?>
+$sql = "INSERT INTO user (id, pw, l_name,f_name) VALUES('$id', '$pw', '$l_name','$f_name')";
 
+if($conn->query($sql))echo "도서등록 성공";
+else echo "도서등록 실패";
 
+?>
--- login/login_action.php
+++ login/login_action.php
@@ -1,23 +1,34 @@
-<?php # form 데이터 읽어오기
-$id = $_POST["id"];
-$pw = $_POST["pw"];
+<?php
+$id=$_POST['id'];
+$pw=$_POST['pw'];
 
 $db_host = "localhost"; 
 $db_user = "bigeducation"; 
 $db_passwd = "dusrntlf0!";
-$db_name = "bigeducation"; 
-$conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
-# insert sql 작성
-$query = "SELECT pw FROM user WHERE id =  '" . $id . "';";
-$result=mysqli_query($con, $query);
+$db_name = "bigeducation";
 
-if(!$result) 
-{?>
+$conn = new mysqli($db_host, $db_user, $db_passwd, $db_name);
 
-    <script> alert('로그인 성공'); location.href=".."; </script> 
-<?php
-} else { ?>
-    <script> alert('로그인 실패 retry.. '); location.href=".."; </script>
-    <?php
- }
+$check="SELECT * FROM user WHERE id ='$id'";
+
+$result=$conn->query($check); 
+if($result->num_rows==1){
+    $row=$result->fetch_array(MYSQLI_ASSOC); //하나의 열을 배열로 가져오기
+    if($row['pw']==$pw){  //MYSQLI_ASSOC 필드명으로 첨자 가능
+        $_SESSION['id']=$id;           //로그인 성공 시 세션 변수 만들기
+        if(isset($_SESSION['id']))    //세션 변수가 참일 때
+        {
+            header('Location: ../index.html');   //로그인 성공 시 페이지 이동
+        }
+        else{
+            echo "세션 저장 실패";
+        }            
+    }
+    else{
+        echo "wrong id or pw";
+    }
+}
+else{
+    echo "wrong id or pw";
+}
 ?>
